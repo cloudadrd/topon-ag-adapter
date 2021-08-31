b613f7b340fdd02f93f48a2305d89fd5b653d781
tianwenshi <100345612@qq.com>
2021-08-31 15:12:38 +0800

    public属性

--- business-support-sdk/src/main/java/com/business/support/calendar/CalendarPara.java
+++ business-support-sdk/src/main/java/com/business/support/calendar/CalendarPara.java
@@ -1,12 +1,12 @@
 package com.business.support.calendar;
 public class CalendarPara {
-    long eventId;
-    String title;
-    String description;
-    long startTime;
-    long endTime ;
-    boolean alarm;
-    int repeatInterval;//重复间隔,只在批量添加时使用
-    int repeatCount; //重复次数,只在批量添加时使用
+    public long eventId;
+    public String title;
+    public String description;
+    public long startTime;
+    public long endTime ;
+    public boolean alarm;
+    public int repeatInterval;//重复间隔,只在批量添加时使用
+    public int repeatCount; //重复次数,只在批量添加时使用
 
 }
